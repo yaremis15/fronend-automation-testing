666f61e5ea4f2a11dafbc29fbf2d886ef3ade71f
Yaremis Martínez <yaremis99@hotmail.com>
2024-02-29 20:06:30 -0500

    refactor: delete configuration files

---
+++ src/test/java/org/frontend/testing/demo/runners/TestSuiteRunner.java
@@ -0,0 +1,18 @@
+package org.fronend.testing.reto.runners;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(publish = true,
+        features = {"src/test/resources/features.demo"},
+        glue = {"org.fronend.testing.reto.steps.hooks"},
+        tags = "@navigateToPageHome",
+        snippets = CAMELCASE)
+
+public class TestSuiteRunner {
+
+}
